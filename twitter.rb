cac8d0d2019bc4cfab3ff06d87397dd00fa08fc2
rebeccabosse <rebeccabosse@hotmail.fr>
2018-11-17 14:40:26 +0100

    edit post twitter

---
+++ twitter.rb
@@ -0,0 +1,23 @@
+# ligne très importante qui appelle la gem. Sans elle, le programme ne saura pas appeler Twitter
+require 'twitter'
+
+# Appelle la gem dotenv
+require 'dotenv'
+
+require 'pry'
+
+# Ceci appelle le fichier .env grâce à la gem dotenv, et enregistre toutes les données enregistrées dans une hash ENV
+Dotenv.load
+
+# quelques lignes qui enregistrent les clés d'APIs
+
+client = Twitter::REST::Client.new do |config|
+ config.consumer_key        = ENV["TWITTER_API_KEY"]
+ config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+ config.access_token        = ENV["TWITTER_USER_TOKEN"]
+ config.access_token_secret = ENV["TWITTER_USER_TOKEN_SECRET"]
+end
+
+
+# ligne qui permet de tweeter
+p client.update('Bonjour monde !')
